311c8b00e110c89aba3d954a91b2821dc41b2b05
igorsilva <igor.silva@performait.com>
2022-01-25 21:38:35 -0300

    remove console

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -7,8 +7,6 @@ interface IExperience{
 const Experience: React.FC <IExperience> = ({year, text}) => {
   
   const renderExperience = () => {
-    console.log('init renderExperience')
-    console.log(text)
     return text.map((item, i) => {        
         return <p key={i}>{item}</p>      
     })
