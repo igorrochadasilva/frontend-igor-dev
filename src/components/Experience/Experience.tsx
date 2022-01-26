485bae55d84bb234b46247609792be547ce1e51f
igorsilva <igor.silva@performait.com>
2022-01-25 21:21:52 -0300

    update portfolio

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 interface IExperience{
   year: string
-  text: string  
+  text: string[]  
 }
 
-const Experience: React.FC <IExperience> = ({year, text}) => (
-  <>
-  <h3>{year}</h3>
-  <p>
-    {text}
-  </p>
-  </>
-)
+const Experience: React.FC <IExperience> = ({year, text}) => {
+  
+  const renderExperience = () => {
+    console.log('init renderExperience')
+    console.log(text)
+    return text.map((item, i) => {        
+        return <p key={i}>{item}</p>      
+    })
+  }
+
+  return(
+    <>
+      <h3>{year}</h3>
+      {renderExperience()}
+    </>
+  )
+
+}
 
 export default Experience;
